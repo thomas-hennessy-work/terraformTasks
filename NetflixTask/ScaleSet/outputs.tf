2697a65f9711962e5ad6e578f4309c96a956a5ca
Ubuntu <tom@terraformMachine.vvbt4t4bsebebbnbnotpis4asc.zx.internal.cloudapp.net>
2021-01-05 15:48:58 +0000

    EOD commit

---
+++ NetflixTask/ScaleSet/outputs.tf
@@ -0,0 +1,3 @@
+output "scaleSetID"{
+    value = azurerm_linux_virtual_machine_scale_set.NetflixVMSS.id
+}
--- NetflixTask/main.tf
+++ NetflixTask/main.tf
@@ -16,6 +16,24 @@ variable "location" {
   }
 }
 
+variable "scaleOutTime" {
+    description = "the time in each region the scale set should scale out"
+    default = {
+    1 = "PT10M"
+    2 = "PT9M"
+    3 = "PT2M30"
+    }
+}
+
+variable "scaleInTime" {
+    description = "the time in each region the scale set should scale in"
+    default = {
+    1 = "PT15M"
+    2 = "PT17M"
+    3 = "PT10M30"
+    }
+}
+
 module "Netflix_Scale_Set" {
   for_each = var.location
 
@@ -23,4 +41,12 @@ module "Netflix_Scale_Set" {
   region            = each.value
   key = each.key
   ResourceGroupName = azurerm_resource_group.NetflixRG.name
+}
+
+module "Netflix_Monitor" {
+    for_each = var.location
+
+    source = "./ScalingMonitor"
+    region = each.value
+    
 }
