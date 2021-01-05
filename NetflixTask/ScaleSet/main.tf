4babffa1a40d099a01c08dd83664755515a272cb
Ubuntu <tom@terraformMachine.vvbt4t4bsebebbnbnotpis4asc.zx.internal.cloudapp.net>
2021-01-05 12:03:06 +0000

    scale set connectable via ssh, variable location

--- NetflixTask/ScaleSet/main.tf
+++ NetflixTask/ScaleSet/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "NetflixRG" {
   name     = "Netflix-Resources"
-  location = "France Central"
+  location = var.region
 }
 
 resource "azurerm_virtual_network" "NetflixVN" {
@@ -19,6 +19,28 @@ resource "azurerm_subnet" "NVNSubnet" {
   resource_group_name  = azurerm_resource_group.NetflixRG.name
   virtual_network_name = azurerm_virtual_network.NetflixVN.name
   address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "NFSecurityGroup" {
+  name                = "NetflixSecurityGroup"
+  location            = azurerm_resource_group.NetflixRG.location
+  resource_group_name = azurerm_resource_group.NetflixRG.name
+
+  security_rule {
+    name                       = "allowSSH"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "NetflixVMSS" {
@@ -49,11 +71,16 @@ resource "azurerm_linux_virtual_machine_scale_set" "NetflixVMSS" {
   network_interface {
     name    = "Network-Interface"
     primary = true
+    network_security_group_id = azurerm_network_security_group.NFSecurityGroup.id
 
     ip_configuration {
       name      = "internal"
       primary   = true
       subnet_id = azurerm_subnet.NVNSubnet.id
+
+      public_ip_address {
+        name = "pubIP"
+      }
     }
   }
 }
